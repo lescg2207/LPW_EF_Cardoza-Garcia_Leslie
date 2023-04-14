82ad324c7e484ab1b316ada1b345f55ac3248d61
lescg2207 <leslieal2202@gmail.com>
2023-04-14 00:23:12 -0500

    presidenciales

--- usp_getResumen.sql
+++ usp_getResumen.sql
@@ -2,10 +2,10 @@
 alter PROCEDURE sp_getResumen_Cardoza_Garcia
 AS
 BEGIN
-SELECT FORMAT(SUM(P1), 'N2') 'TotalVotosPPK', FORMAT(SUM(P2), 'N2') ' TotalVotosKeiko', CONCAT(CAST((SUM(TotalVotantes)-SUM(P1)) * 100.0 / SUM(TotalVotantes) AS decimal(10, 3)), ' %') '% VOTOS VÁLIDOSppk',
-CONCAT(CAST((SUM(P2)) * 100.0 / SUM(TotalVotantes)  AS decimal(8, 3)), ' %' ) '% VOTOS VÁLIDOSkeyko',
-format(COUNT(idEstadoActa),'N2') 'totalActas', format(COUNT(*),'N2')'totalActasProcesadas',format(COUNT(*),'N2') 'totalActasContabilizadas', FORMAT(SUM(ElectoresHabiles), 'N2')  'Electores Hábiles', FORMAT(SUM(TotalVotantes), 'N2') 'Ciudadanos que votaron ',
-CONCAT ( CAST ( ( SUM(TotalVotantes) * 100.0 / SUM(TotalVotantes) ) as decimal (8,3) ), ' %' ) '% de participacion',
+SELECT FORMAT(SUM(P1), 'N0') 'TotalVotosPPK', FORMAT(SUM(P2), 'N0') ' TotalVotosKeiko', CONCAT(CAST((SUM(P1)) * 100.0 / SUM(TotalVotantes-(VotosBlancos+VotosNulos)) AS decimal(10, 3)), ' %') '% VOTOS VÁLIDOSppk',
+CONCAT(CAST((SUM(P2)) * 100.0 / SUM(TotalVotantes-(VotosBlancos+VotosNulos))  AS decimal(8, 3)), ' %' ) '% VOTOS VÁLIDOSkeyko',
+format(COUNT(idEstadoActa),'N0') 'totalActas', format(COUNT(*),'N0')'totalActasProcesadas',format(COUNT(*),'N0') 'totalActasContabilizadas', FORMAT(SUM(ElectoresHabiles), 'N0')  'Electores Hábiles', FORMAT(SUM(TotalVotantes), 'N0') 'Ciudadanos que votaron ',
+CONCAT ( CAST ( ( SUM(TotalVotantes) * 100.0 / SUM(ElectoresHabiles) ) as decimal (8,3) ), ' %' ) '% de participacion',
 CONCAT ( CAST ( ( ( SUM(ElectoresHabiles) - SUM(TotalVotantes) ) * 100.0 / SUM(ElectoresHabiles) ) as decimal (8,3) ), ' %' ) '% de ausentismo'
 FROM GrupoVotacion
 END
